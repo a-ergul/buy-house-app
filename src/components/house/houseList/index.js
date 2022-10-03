1999499ae21a1a200bf2b23b0f8f2bf47913f9dc
a-ergul <alpyergul@gmail.com>
2022-10-03 21:48:38 +0300

    #4 Feat: Get House List

--- src/components/house/houseList/index.js
+++ src/components/house/houseList/index.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import { HouseCont } from '../houseContext';
+import House from '../house';
+
+import { ImSpinner2 } from 'react-icons/im';
+
 
 const HouseList = () => {
+  const { houses, loading} = useContext(HouseCont);
+
+  if (loading) {
+    return (
+      <ImSpinner2/>
+    )
+  }
+
+  if (houses.length < 1) {
+    return (
+      <div className='text-center'>Sorry</div>
+    )
+  }
   return (
-    <div></div>
+    <section className='mb-20'>
+      <div className='container mx-auto'>
+        <div className='grid md:grid-cols-2 lg:grid-cols-5 gap-4 lg:gap-14'>
+          {houses.map((house)=> {
+            return (
+              <House house={house}/>
+            )
+          })}
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Banner from '../../components/banner'
 import HouseContextProv from '../../components/house/houseContext'
+import HouseList from '../../components/house/houseList'
 
 const Dashboard = () => {
   return (
     <div className='min-h-[1800px]'>
       <Banner/>
-      <HouseContextProv/>
+      <HouseList/>
     </div>
   )
 }
